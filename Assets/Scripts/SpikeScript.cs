91bdbb7f9d6a90269d1397fa6704a5f2a90ea7b2
ThrDreen <callumlegendre@gmail.com>
2025-11-03 14:36:55 -0500

    script for spike trap prefab
    created and formatted the script for the spike trap prefab

---
+++ Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,56 @@
+// ===========================================================================================================
+// File: SpikeScript.cs
+// Author: Callum Legendre
+// Date: November 3, 2025
+// Description: Script for the spike trap prefab. Handles specifics of damage dealing and deactivation.
+// ===========================================================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDB
+{
+    public class SpikeScript : MonoBehaviour
+    {
+        // ================================
+        // Fields
+        // ================================
+
+
+        // ================================
+        // Properties
+        // ================================
+
+
+        // ================================
+        // Unity Lifecycle Methods
+        // ================================
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        // ================================
+        // Public Methods
+        // ================================
+
+
+        // ================================
+        // Private Methods
+        // ================================
+
+
+        // ================================
+        // Events and Handlers
+        // ================================
+    }
+}
