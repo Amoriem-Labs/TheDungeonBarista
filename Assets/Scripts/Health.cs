6f24a2c34396bede34f730fc075244e62b551bcf
ThrDreen <callumlegendre@gmail.com>
2026-01-18 18:07:54 -0500

    edited health script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,62 +3,82 @@
 // Author: Ryan Lin
 // Date: October 25, 2025
 // Description: Handles health, damage taken, and die features
+
+
+// NOTE: I (Callum Legendre) redid a bunch of the logic here, if you wanna see the old one look at the 'dungeon-hp-dmg' branch'
 // =====================================================================================
 
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TDB
 {
     public class Health : MonoBehaviour
     {
-        [Header("Health Settings")]
-        public float MaxHealth = 100f;
-        private float _currentHealth;
+        // =============
+        // Fields
+        // ==============
+
+        // intialisation of max mana and current mana fields
+        [SerializeField] int MaxHealth = 100;
+        
+        // the variable where the referance to the UI element is stored. All operations to adjust mana happen on the value element of the slider.
+        [SerializeField] Slider HealthBar;
 
         // Start is called before the first frame update
         void Start()
         {
-            _currentHealth = MaxHealth;
+            // gets the slider component
+            HealthBar = GetComponent<Slider>();
+            
+            // sets maximum and minimum value of the bar, along with only using whole numbers
+            HealthBar.maxValue = MaxHealth;
+            HealthBar.minValue = 0;
+            HealthBar.wholeNumbers = true;
+            
+            // spawns the player in with a full health bar
+            HealthBar.value = MaxHealth;
         }
         
         // temporary for testing health bar
         void Update()
         {
-            // Press Space to take 10 damage
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                TakeDamage(10f);
-                Debug.Log("Took 10 damage, current HP: " + _currentHealth);
-            }
+
         }
+
+        // ==============
+        // Public Methods
+        // ==============
 
         public void TakeDamage(float amount)
         {
-            _currentHealth -= amount;
+            HealthBar.value -= amount;
 
             // Makes sure HP doesn't go below 0
-            if (_currentHealth <= 0)
+            if (HealthBar.value <= 0)
             {
-                _currentHealth = 0;
+                HealthBar.value = 0;
                 // Die();
             }
         }
 
         public void RegenHealth(float amount)
         {
-            _currentHealth += amount;
+            HealthBar.value += amount;
 
             // Makes sure HP doesn't go above MaxHealth
-            if (_currentHealth > MaxHealth)
-                _currentHealth = MaxHealth;
+            if (HealthBar.value > MaxHealth) 
+            {
+                HealthBar.value = MaxHealth;
+            }
         }
 
         public float GetCurrentHealth()
         {
-            return _currentHealth;
+            return HealthBar.value;
         }
 
         private void Die()
